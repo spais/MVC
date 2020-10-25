ccebdeac5e0fdba3475064c396ef2eafbabdf30a
Shawn Pais <pais.the.designer@gmail.com>
2020-10-25 15:22:55 -0400

    modal complete

--- script.js
+++ script.js
@@ -1,6 +1,51 @@
 class Model {
     constructor() {
 
+        // The state of the model with prepopulated array of todos objects
+        this.todos = [
+            {
+                id: 1, 
+                text: 'Better understand MVC',  
+                complete: false
+            },
+            {
+                id: 2, 
+                text: 'Deep dive js prototypes', 
+                complete: false
+            },
+        ]
+
+    }
+
+    addTodo(todoText) {
+        const todo = {
+            id: this.todos.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1,
+            text: todoText,
+            complete: false,
+        }
+
+        this.todos.push(todo)
+    }
+
+    // Used to map through every todos and replaces the text of the todo with the specified id
+    editTodo(id, updatedText) {
+        this.todos = this.todos.map((todo) =>
+        todo.id === id ? {id: todo.id, text: updatedText, complete: todo.complete} : todo,
+        )
+    }
+
+    // Filter a todo out of the array by id
+    deleteTodo(id) {
+        this.todos = this.todos.filter((todo) => todo.id !== id)
+    }
+
+    // Flip the complete boolean on the specified todo
+    toggleTodo(id) {
+        this.todos = this.todos.map((todo) =>
+        todo.id === id ? {id: todo.id, text: todo.text, complete: !todo.complete} : todo,
+        )
+    }
+
 }
 
 class View {
